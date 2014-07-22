931da15e118b07c579e28860718b709924a27728
fmontei <felipe.carneiro.monteiro@gmail.com>
2014-07-21 20:37:47 -0400

    Finished student evaluation of tutor.

--- php/student_eval.php
+++ php/student_eval.php
@@ -2,7 +2,7 @@
   session_start();
 
   $database = "4400_project_db";
-  $con = mysql_connect(localhost, "root", "mysql");
+  $con = mysql_connect("localhost", "root", "mysql");
   @mysql_select_db($database) or die("Unable to select database");
 
   if (strpos($_SERVER["QUERY_STRING"], "populate_table") !== false) {
@@ -48,12 +48,28 @@
   }
 
   function processReview() {
+    $user_gtid = $_SESSION['user_gtid'];
     $tutor_gtid = $_GET["tutorGTIDSelection"];
     $tutor_name = $_GET["tutorNameSelection"];
     $school = $_GET["tutorSchoolSelection"];
     $courseNum = $_GET["tutorCourseSelection"];
+    $desc_eval = $_GET["desc_eval"];
+    $num_eval = $_GET["final_num_eval_input"];
 
-    print($tutor_gtid . " " . $tutor_name . " " . $school . " " . $courseNum);
+    $query = sprintf("INSERT INTO Rates(GTID_Undergraduate, GTID_Tutor, School, " .
+      "Number, Num_Evaluation, Desc_Evaluation) VALUES('%s', '%s', '%s', '%s', '%s', '%s');",
+      mysql_real_escape_string($user_gtid), mysql_real_escape_string($tutor_gtid),
+      mysql_real_escape_string($school), mysql_real_escape_string($courseNum),
+      mysql_real_escape_string($num_eval), mysql_real_escape_string($desc_eval));
+    $result = mysql_query($query);
+    if (!$result) {
+      $message  = 'Invalid query: ' . mysql_error() . "\n";
+      $message .= 'Whole query: ' . $query;
+      die($message);
+    } else {
+      header("Location: ../html/menu.html");
+      die();
+    }
   }
 
 ?>
