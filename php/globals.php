6f96a8b5fd1435202cdaf562c36d1a29ab55f12c
Felipe <fmontei@g.clemson.edu>
2015-08-25 22:58:41 -0400

    Changed DB password to generic password.

--- php/globals.php
+++ php/globals.php
@@ -1,7 +1,7 @@
 <?php
   function db_connect() {
     $database = "GT-Tutor-App";
-    $con = mysql_connect("georgia-tech-tutor-app.cijyhxa0crol.us-east-1.rds.amazonaws.com:3306", "admin", "13prometheu~31");
+    $con = mysql_connect("georgia-tech-tutor-app.cijyhxa0crol.us-east-1.rds.amazonaws.com:3306", "admin", "password");
     @mysql_select_db($database) or die("Unable to select database");
   }
 ?>
